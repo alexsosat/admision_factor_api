de972532aad6ce3e51df4d807fab1ef7b345d223
Alex <1080810@alumno.um.edu.mx>
2021-10-20 11:48:36 -0500

    new model api

--- app.py
+++ app.py
@@ -12,35 +12,111 @@ def index_page():
 
 @app.route("/result", methods=['POST'])
 def post_data():
+    content = request.form
     if request.method == "POST":
-        gre = request.form["gre"]
-        gpa = request.form["gpa"]
-        income = request.form["income"]
-        gender = request.form["gender"]
-        ethnic = request.form["ethnic"]
-        rank = request.form["prestige"]
+        radius_mean = content["radius_mean"]
+        radius_se = content["radius_se"]
+        radius_worst = content["radius_worst"]
+
+        texture_mean = content["texture_mean"]
+        texture_se = content["texture_se"]
+        texture_worst = content["texture_worst"]
+
+        perimeter_mean = content["perimeter_mean"]
+        perimeter_se = content["perimeter_se"]
+        perimeter_worst = content["perimeter_worst"]
+
+        area_mean = content["area_mean"]
+        area_se = content["area_se"]
+        area_worst = content["area_worst"]
+
+        smoothness_mean = content["smoothness_mean"]
+        smoothness_se = content["smoothness_se"]
+        smoothness_worst = content["smoothness_worst"]
+
+        compactness_mean = content["compactness_mean"]
+        compactness_se = content["compactness_se"]
+        compactness_worst = content["compactness_worst"]
+
+        concavity_mean = content["concavity_mean"]
+        concavity_se = content["concavity_se"]
+        concavity_worst = content["concavity_worst"]
+
+        points_mean = content["points_mean"]
+        points_se = content["points_se"]
+        points_worst = content["points_worst"]
+
+        symmetry_mean = content["symmetry_mean"]
+        symmetry_se = content["symmetry_se"]
+        symmetry_worst = content["symmetry_worst"]
+
+        fractal_dimension_mean = content["fractal_dimension_mean"]
+        fractal_dimension_se = content["fractal_dimension_se"]
+        fractal_dimension_worst = content["fractal_dimension_worst"]
 
         class_prediction = model.model_prediction(
-            gre, gpa, income, gender, ethnic, rank)
+            radius_mean, texture_mean, perimeter_mean, area_mean, smoothness_mean, compactness_mean,
+            concavity_mean, points_mean, symmetry_mean,
+            fractal_dimension_mean, radius_se, texture_se, perimeter_se, area_se, smoothness_se,
+            compactness_se, concavity_se, points_se, symmetry_se, fractal_dimension_se, radius_worst,
+            texture_worst, perimeter_worst, area_worst, smoothness_worst, compactness_worst, concavity_worst,
+            points_worst, symmetry_worst, fractal_dimension_worst
+        )
 
     return render_template("result.html", n=class_prediction)
 
 
-@app.route("/model_prediction", methods=['POST'])
+@app.route("/breast_cancer_prediction", methods=['POST'])
 def api_call():
     content = request.json
     if request.method == "POST":
-        gre = content["gre"]
-        gpa = content["gpa"]
-        income = content["income"]
-        gender = content["gender"]
-        ethnic = content["ethnic"]
-        rank = content["prestige"]
+        radius_mean = content["radius"]["mean"]
+        radius_se = content["radius"]["se"]
+        radius_worst = content["radius"]["worst"]
+
+        texture_mean = content["texture"]["mean"]
+        texture_se = content["texture"]["se"]
+        texture_worst = content["texture"]["worst"]
+
+        perimeter_mean = content["perimeter"]["mean"]
+        perimeter_se = content["perimeter"]["se"]
+        perimeter_worst = content["perimeter"]["worst"]
+
+        area_mean = content["area"]["mean"]
+        area_se = content["area"]["se"]
+        area_worst = content["area"]["worst"]
+
+        smoothness_mean = content["smoothness"]["mean"]
+        smoothness_se = content["smoothness"]["se"]
+        smoothness_worst = content["smoothness"]["worst"]
+
+        compactness_mean = content["compactness"]["mean"]
+        compactness_se = content["compactness"]["se"]
+        compactness_worst = content["compactness"]["worst"]
+
+        concavity_mean = content["concavity"]["mean"]
+        concavity_se = content["concavity"]["se"]
+        concavity_worst = content["concavity"]["worst"]
+
+        points_mean = content["points"]["mean"]
+        points_se = content["points"]["se"]
+        points_worst = content["points"]["worst"]
+
+        symmetry_mean = content["symmetry"]["mean"]
+        symmetry_se = content["symmetry"]["se"]
+        symmetry_worst = content["symmetry"]["worst"]
+
+        fractal_dimension_mean = content["fractalDimensions"]["mean"]
+        fractal_dimension_se = content["fractalDimensions"]["se"]
+        fractal_dimension_worst = content["fractalDimensions"]["worst"]
 
         class_prediction = model.model_prediction(
-            gre, gpa, income, gender, ethnic, rank)
+            radius_mean, texture_mean, perimeter_mean, area_mean, smoothness_mean, compactness_mean,
+            concavity_mean, points_mean, symmetry_mean,
+            fractal_dimension_mean, radius_se, texture_se, perimeter_se, area_se, smoothness_se,
+            compactness_se, concavity_se, points_se, symmetry_se, fractal_dimension_se, radius_worst,
+            texture_worst, perimeter_worst, area_worst, smoothness_worst, compactness_worst, concavity_worst,
+            points_worst, symmetry_worst, fractal_dimension_worst
+        )
 
-    if class_prediction == 0:
-        return "0"
-    else:
-        return "1"
+        return class_prediction
--- model.py
+++ model.py
@@ -5,15 +5,17 @@ import pandas as pd
 from sklearn import tree
 
 
-def model_prediction(gre, gpa, ses, gender, ethnic, rank):
-    W_P = pd.read_csv('Admission2.csv')
-    X_prime = W_P.iloc[:, [1, 2, 3, 4, 5, 6]].values
-    Y = W_P.iloc[:, 0].values
+def model_prediction(radius_mean, texture_mean, perimeter_mean, area_mean, smoothness_mean, compactness_mean, concavity_mean, points_mean, symmetry_mean, fractal_dimension_mean, radius_se, texture_se, perimeter_se, area_se, smoothness_se, compactness_se, concavity_se, points_se, symmetry_se, fractal_dimension_se, radius_worst, texture_worst, perimeter_worst, area_worst, smoothness_worst, compactness_worst, concavity_worst, points_worst, symmetry_worst, fractal_dimension_worst):
+    W_P = pd.read_csv('breast-cancer.csv')
+    X_prime = W_P.iloc[:, [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+                           16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]].values
+    Y = W_P.iloc[:, 1].values
 
     ModelosTree = tree.DecisionTreeClassifier(max_leaf_nodes=20)
     ModelosTree.fit(X_prime, Y)
 
-    X_test = np.array([gre, gpa, ses, gender, ethnic, rank])
+    X_test = np.array([radius_mean, texture_mean, perimeter_mean, area_mean, smoothness_mean, compactness_mean, concavity_mean, points_mean, symmetry_mean, fractal_dimension_mean, radius_se, texture_se, perimeter_se, area_se, smoothness_se,
+                      compactness_se, concavity_se, points_se, symmetry_se, fractal_dimension_se, radius_worst, texture_worst, perimeter_worst, area_worst, smoothness_worst, compactness_worst, concavity_worst, points_worst, symmetry_worst, fractal_dimension_worst])
     X_test = X_test.reshape((1, -1))
 
     return ModelosTree.predict(X_test)[0]
